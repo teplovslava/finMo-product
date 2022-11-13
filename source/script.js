97d7bcd3e0c94adaeda0438be99f28de8968dad6
teplovslava <slava.teplov.2014@mail.ru>
2022-11-13 21:34:49 +0300

    add

---
+++ source/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const list = document.querySelectorAll('.questions-list li')
+    
+    
+    list.forEach(item=>{
+        item.classList.remove('active')
+        item.addEventListener('click',function(){
+                list.forEach(li=>li.classList.remove('active'))
+                item.classList.add('active')
+    })}
+        
+    )
+    const menuItem = document.querySelectorAll('.menu li')
+    const close = document.querySelector('.burger-close')
+    const burger = document.querySelector('.burger')
+    const openBurger1 = document.querySelectorAll('.nav-item')[0]
+    const openBurger2 = document.querySelectorAll('.nav-item')[1]
+
+  
+    menuItem.forEach(item=>{
+        item.classList.remove('active')
+        item.addEventListener('click',function(){
+          menuItem.forEach(li=>li.classList.remove('active'))
+                item.classList.add('active')
+    })}
+        
+    )
+    
+
+    close.addEventListener('click', function(){
+      burger.classList.add('display-none')
+      menuItem.forEach(li=>li.classList.remove('active'))
+    })
+
+    
+    openBurger1.addEventListener('click', function(){
+      burger.classList.remove('display-none')
+    })
+
+    openBurger2.addEventListener('click', function(){
+      burger.classList.remove('display-none')
+    })
+    
+})
